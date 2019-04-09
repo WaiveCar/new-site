c6ee4a468dfb2b0cbd1b1a1050f6eb50a7056a27
David Leigh <daleighan@gmail.com>
2019-04-09 16:18:21 -0700

    Changed paths to match what they are in prod + fixed links

--- src/partials/header.php
+++ src/partials/header.php
@@ -82,9 +82,8 @@
         <nav class="h-menu">
         <?
           $header = [
-            [ 'index.html', 'Waive Work' ],
             [ 'waive-car.html', 'Waive Car' ],
-            [ "how-it-works.html", 'How it Works' ],
+            //[ "how-it-works.html", 'How it Works' ],
             [ "faq", 'FAQ' ],
             [ 'about.html', 'About us' ]
           ];
---
+++ src/partials/work-info.php
@@ -0,0 +1,325 @@
+<? 
+$PAGETITLE = 'Waive Work';
+include('header.php'); 
+?>    
+    
+    <section class="work-main">
+      
+      <div class="work-main-info">
+        
+        <div class="work-main-info__textual">
+          
+          <h1 class="work-main-info__title">Drive More, Pay Less</h1>
+          
+          <p class="work-main-info__desc">WaiveWork provides affordable cars for people with driving centered careers.</p>
+        
+        </div>
+        
+        <a class="work-main-btn" href="/index.html">
+          <span class="work-main-btn__title">Get Started Now</span>
+          
+          <svg class="work-main-btn__icon">
+            <use xlink:href="#icon-classic-arrow"></use>
+          </svg>
+        </a>
+      
+      </div>
+      
+      <div class="work-main-picture">
+        <img src="pic/work-main-whitewrap.jpg" alt="Hyundai IONIQ">
+      </div>
+    
+    </section>
+    <section class="work-why">
+      <div class="work-why__wrapper">
+        
+        <h2 class="work-why__title">Why WaiveWork?</h2>
+        
+        <p class="work-why__desc"> Opportunities to make money driving are increasing. The problem? So are the costs of car ownership. Our solution
+          is WaiveWork: the world’s first car rental service funded by advertising!</p>
+      
+      </div>
+    </section>
+    
+    <section class="work-for-me">
+      <div class="work-for-me__wrapper">
+        
+        <h2 class="work-for-me__title">Is WaiveWork For Me?</h2>
+        
+        <p class="work-for-me__desc">Whether you don’t have a car, or are simply searching for a more cost efficient work vehicle - WaiveWork is for
+          you.</p>
+        
+        <div class="work-for-me__container">
+          
+          <div class="work-for-me-item">
+            
+            <!-- Icon -->
+            <div class="work-for-me-item__icon work-for-me-item__icon--1">
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 50 50">
+                <g fill="none" fill-rule="evenodd" stroke="#56665A" stroke-linecap="round" stroke-linejoin="round" stroke-width="2">
+                  <path d="M1.8 45h6.4s3.547-12.411 4-17.6c-1.597-1.514-10.4-1.6-10.4-1.6V45z"/>
+                  <path d="M34.078 39.194l7.728-1.932a5.622 5.622 0 0 1 6.394 2.941l-11.096 5.043c-8.405 3.447-9.109 3.53-13.112 2.018L8.86 42.219"/>
+                  <path d="M11.887 30.115l18.157 4.036c2.694.673 4.035 1.947 4.035 4.724v.319l-2.715.678a6.067 6.067 0 0 1-2.655.064l-8.752-1.752m26.378-.384c.22-.995.334-2.03.334-3.091 0-5.543-12.068-20.109-12.068-20.109h-8s-9.082 10.536-11.2 16.042M22.6 14h16"/>
+                  <path d="M26.6 14.6S15.394 6.126 17.8 3.4c1.493-1.691 8 3.2 8 3.2s3.373-4.8 4.8-4.8c1.427 0 4.8 4.8 4.8 4.8s6.507-4.891 8-3.2c2.406 2.726-8.8 11.2-8.8 11.2h-8zm6.384 20c.507-.502.816-1.165.816-1.89 0-2.312-2.639-2.7-3.197-2.836-.559-.136-3.203-.525-3.203-2.837 0-1.568 1.435-2.837 3.203-2.837 1.702 0 3.091 1.174 3.197 2.654M30 22.6v11.2"/>
+                </g>
+              </svg>
+            </div>
+            
+            <!-- Title -->
+            <div class="work-for-me-item__title">Want to save money?</div>
+          
+          </div>
+          
+          <div class="work-for-me-item">
+            
+            <div class="work-for-me-item__icon work-for-me-item__icon--2">
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 66 38">
+                <g fill="none" fill-rule="evenodd" stroke="#56665A" stroke-linecap="round" stroke-linejoin="round" stroke-width="2">
+                  <path d="M35 4.889V2.11C35 1.56 34.38 1 33.759 1H18.24C17.621 1 17 1.56 17 2.111V4.89C17 5.44 17.62 6 18.241 6H33.76C34.379 6 35 5.44 35 4.889zM18.655 30.103h28.69m11.034 0h.647c3.3 0 5.974-2.655 5.974-5.931 0-2.9-2.11-5.374-4.992-5.85l-5.298-.877a11.067 11.067 0 0 1-5.25-2.39L41.586 8.54A11.08 11.08 0 0 0 34.522 6H19.056a9.58 9.58 0 0 0-8.564 5.256l-2.871 5.7H6.31C3.378 16.956 1 19.317 1 22.23v2.602c0 2.911 2.378 5.272 5.31 5.272h1.31"/>
+                  <path d="M43.814 10.241H20.29a6.218 6.218 0 0 0-5.561 3.438l-.59 1.177a1.386 1.386 0 0 0 1.241 2.006h37.49m-29.802 0v-6.62m8.828 6.62v-6.62m28.69 13.24h4.192m-30.676-2.206h2.207m-17.655 9.93a5.518 5.518 0 1 1-11.036 0 5.518 5.518 0 0 1 11.036 0z"/>
+                  <path d="M14.241 31.207a1.104 1.104 0 1 1-2.207 0 1.104 1.104 0 0 1 2.207 0zm39.725 0a1.104 1.104 0 1 1-2.208 0 1.104 1.104 0 0 1 2.208 0z"/>
+                  <path d="M58.38 31.207a5.518 5.518 0 1 1-11.036-.001 5.518 5.518 0 0 1 11.035 0z"/>
+                </g>
+              </svg>
+            </div>
+            
+            <!-- Title -->
+            <div class="work-for-me-item__title">Can’t afford to buy car?</div>
+          
+          </div>
+          
+          <div class="work-for-me-item">
+            
+            <div class="work-for-me-item__icon work-for-me-item__icon--3">
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 47 48">
+                <g fill="none" fill-rule="evenodd" stroke="#56665A" stroke-linecap="round" stroke-linejoin="round" stroke-width="2">
+                  <path d="M40.95 34.426L32.971 31c-2.57 2.938-5.903 4.666-9.501 4.666-3.6 0-6.934-1.728-9.503-4.666L5.99 34.426c-1.803.774-3.117 2.443-3.499 4.445L1 47h45l-1.552-8.13c-.381-2-1.696-3.67-3.499-4.444zM18 19h2m8 0h2"/>
+                  <path d="M12.61 11c-.16.738-.64 1.303-.64 2.088 0 .342.049.67.085 1.003-.03-.004-.057-.018-.085-.018-1.09 0-1.97 1.764-1.97 3.94 0 2.177.88 3.94 1.97 3.94.201 0 .394-.079.578-.192C13.995 27.597 18.428 32 23.197 32c4.767 0 8.807-4.206 10.255-10.042.184.113.375.192.578.192 1.088 0 1.97-1.763 1.97-3.94 0-2.176-.882-3.94-1.97-3.94-.03 0-.056.014-.085.018.035-.333.085-.66.085-1.003 0-.785-.088-1.547-.246-2.285"/>
+                  <path d="M35 11c-.896 1.52-4.196 4-10.795 4-6.6 0-10.308-2.48-11.205-4M23.5 1C18.66 1 13.102 6.833 10 6.833v2.5C10 10.483 11.286 11 12.382 11h22.236C35.714 11 37 10.067 37 8.917V6.833C33.898 6.833 28.34 1 23.5 1z"/>
+                  <path d="M23 4v1.25c0 .69.31.75 1 .75s1-.06 1-.75V4h-2zM33 39h3v1.5L34.5 42 33 40.5zM20 36v11m8-11v11"/>
+                </g>
+              </svg>
+            </div>
+            
+            <!-- Title -->
+            <div class="work-for-me-item__title">Driving centered career?</div>
+          
+          </div>
+          
+          <div class="work-for-me-item">
+            
+            <div class="work-for-me-item__icon work-for-me-item__icon--4">
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 41 48">
+                <g fill="#56665A" fill-rule="evenodd">
+                  <path d="M25 6H11a1 1 0 0 0-1 1v6a1 1 0 0 0 1 1h14a1 1 0 0 0 1-1V7a1 1 0 0 0-1-1zm-1 6H12V8h12v4zm1 12a1 1 0 0 0-1 1v6a1 1 0 0 0 2 0v-6a1 1 0 0 0-1-1z"/>
+                  <path d="M38.31 11.266a2.996 2.996 0 0 0-3.922-2.448L32 9.612V5c0-2.758-2.244-5-5-5H9C6.244 0 4 2.242 4 5v41H1a1 1 0 0 0 0 2h34a1 1 0 0 0 0-2h-3v-6.054a9.755 9.755 0 0 0 8.67-10.99l-2.36-17.69zM6 46V5c0-1.654 1.346-3 3-3h18c1.654 0 3 1.346 3 3v41H6zm30.834-10.646c-1.25 1.426-2.978 2.296-4.834 2.54V11.722l3.022-1.006a.958.958 0 0 1 .842.1c.254.158.422.42.462.716l2.36 17.69a7.747 7.747 0 0 1-1.852 6.132z"/>
+                </g>
+              </svg>
+            </div>
+            
+            <!-- Title -->
+            <div class="work-for-me-item__title">Car gets bad mileage?</div>
+          
+          </div>
+          
+          <!-- Item -->
+          <div class="work-for-me-item">
+            
+            <div class="work-for-me-item__icon work-for-me-item__icon--5">
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 48 44">
+                <g fill="#56665A" fill-rule="evenodd">
+                  <path d="M1 44h26a1 1 0 0 0 1-1V29c0-3.86-3.14-7-7-7h-3v-4.102c2.28-.464 4-2.484 4-4.898V6h.916a1 1 0 0 0 1-1V1a1 1 0 0 0-1-1h-18a1 1 0 0 0-1 1v4a1 1 0 0 0 1 1H6v7a5.006 5.006 0 0 0 4 4.898V22H7c-3.86 0-7 3.14-7 7v14a1 1 0 0 0 1 1zM5.916 2h16v2h-16V2zM18.78 6C17.358 7.298 15.712 8 14 8c-1.712 0-3.358-.702-4.78-2h9.56zM8 13V7.588C9.762 9.154 11.826 10 14 10s4.238-.846 6-2.412V13c0 1.654-1.346 3-3 3h-6c-1.654 0-3-1.346-3-3zm3 11a1 1 0 0 0 1-1v-5h4v5a1 1 0 0 0 1 1h2.402c-.966 2.404-3.05 4-5.402 4-2.352 0-4.436-1.596-5.402-4H11zm-9 5c0-2.59 1.988-4.702 4.516-4.95C7.63 27.584 10.588 30 14 30s6.37-2.416 7.484-5.95C24.012 24.298 26 26.41 26 29v13H2V29z"/>
+                  <path d="M13 14h2a1 1 0 0 0 0-2h-2a1 1 0 0 0 0 2zm29.754-8.8a.987.987 0 0 0-.924-.012.995.995 0 0 0-.54.75L40.132 14h-3.866l-1.178-8.242a1 1 0 0 0-1.422-.76C30.226 6.64 28 10.172 28 14a9.99 9.99 0 0 0 4 7.998V38c0 3.308 2.692 6 6 6s6-2.692 6-6V21.998A9.99 9.99 0 0 0 48 14a9.98 9.98 0 0 0-5.246-8.8zm-.31 15.45a.994.994 0 0 0-.444.83V38c0 2.206-1.794 4-4 4s-4-1.794-4-4V21.48a1 1 0 0 0-.444-.832A7.99 7.99 0 0 1 30 14a8.015 8.015 0 0 1 3.322-6.48l1.088 7.622a1 1 0 0 0 .99.858H41c.498 0 .918-.366.99-.858l1.056-7.356A7.99 7.99 0 0 1 46 14a7.996 7.996 0 0 1-3.556 6.65z"/>
+                  <circle cx="38" cy="38" r="2"/>
+                </g>
+              </svg>
+            </div>
+            
+            <!-- Title -->
+            <div class="work-for-me-item__title">High car maintenance?</div>
+          
+          </div>
+        
+        </div>
+      
+      </div>
+    </section>
+    
+    
+    <section class="work-how">
+      <div class="work-how__wrapper">
+        
+        <!-- Title -->
+        <h2 class="work-how__title">How it works</h2>
+        
+        <!-- Container -->
+        <div class="work-how__container">
+          
+          <!-- Apply -->
+          <div class="work-how-item">
+            
+            <!-- Icon -->
+            <div class="work-how-item__icon work-how-item__icon--1">
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 73 80">
+                <g fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round" stroke-width="2">
+                  <path stroke="#1DBB3E" d="M72 68.935V3.516A2.509 2.509 0 0 0 69.5 1h-5C63.12 1 62 2.127 62 3.516v65.42L67 79l5-10.065zM62 8h10M62 69h10m-5-56v12"/>
+                  <path stroke="#56665A" d="M5 79h45a4 4 0 0 0 4-4V5a4 4 0 0 0-4-4H5a4 4 0 0 0-4 4v70a4 4 0 0 0 4 4zm5-69h13m-13 5h35m-35 5h35m-35 5h35m-35 5h35m-35 5h35m-35 5h35m-35 5h35m-35 5h35m-35 5h35m-13 5h13"/>
+                  <path stroke="#1DBB3E" d="M9 66.242l1.846 4.652c.187.475.484 1.028 1.078 1.099.704.084 1.184-.57 1.42-1.128A13.249 13.249 0 0 0 14.318 64a7.527 7.527 0 0 0 .182 5.26c.24-.476.477-.952.715-1.428.183.656.388 1.344.947 1.829.557.485 1.593.658 2.159.177.419-.357.434-.917.382-1.414a9.892 9.892 0 0 0-1.24-3.832c.236.767.48 1.548.976 2.224.497.676 1.292 1.245 2.235 1.35.412.046.875-.014 1.15-.274.276-.259.232-.746-.153-.879"/>
+                </g>
+              </svg>
+            </div>
+            
+            <h3 class="work-how-item__title">Apply</h3>
+            
+            <p class="work-how-item__desc">Apply to become a WaiveWork driver. After your interview and orientation, the keys are yours!</p>
+          
+          </div>
+          
+          <div class="work-how-item">
+            
+            <div class="work-how-item__icon work-how-item__icon--2">
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 82 82">
+                <g fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round" stroke-width="2">
+                  <path stroke="#56665A" d="M1 81s48.276-24.483 48.276-38.62c0-8.794-9.655-11.04-9.655-22.07C39.62 9.281 63.069 1 63.069 1m-9.655 80s17.93-24.483 17.93-38.62c0-16.552-9.654-15.922-9.654-22.07C61.69 14.163 81 6.517 81 6.517"/>
+                  <path stroke="#56665A" stroke-dasharray="2 8" d="M27.207 81S58.93 57.897 58.93 43.759c0-16.552-8.276-12.782-8.276-22.07C50.655 12.404 76.862 1 76.862 1"/>
+                  <path stroke="#1DBB3E" d="M15 51v14m8.069-22.62a8.277 8.277 0 0 1-8.276 8.275 8.277 8.277 0 0 1-8.276-8.276c0-4.57 3.706-22.069 8.276-22.069s8.276 17.5 8.276 22.07zm-12.014.185c.18 1.422.917 2.115 1.978 2.382m3.173-12.982c-.804-1.186-1.774-1.469-2.843-1.227"/>
+                </g>
+              </svg>
+            </div>
+            
+            <h3 class="work-how-item__title">Drive</h3>
+            
+            <!-- Description -->
+            <p class="work-how-item__desc"> Drive the car around to earn WaivePoints. Once you hit your weekly quota, your rental will
+              automatically renew.</p>
+          
+          </div>
+          
+          <div class="work-how-item">
+            
+            <div class="work-how-item__icon work-how-item__icon--3">
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 82 67">
+                <g fill="none" fill-rule="evenodd">
+                  <path stroke="#56665A" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M40 25.667v-3.334c0-.661-.758-1.333-1.517-1.333H19.517c-.759 0-1.517.672-1.517 1.333v3.334c0 .661.758 1.333 1.517 1.333h18.966c.759 0 1.517-.672 1.517-1.333zM23 57h36m13.724 0h.808C77.657 57 81 53.695 81 49.617c0-3.61-2.639-6.688-6.24-7.282l-6.622-1.09a13.862 13.862 0 0 1-6.563-2.976l-9.843-8.108A13.885 13.885 0 0 0 42.902 27H23.57c-4.534 0-8.679 2.532-10.705 6.541l-3.59 7.095H7.638C3.972 40.636 1 43.575 1 47.2v3.238C1 54.061 3.972 57 7.637 57h1.639"/>
+                  <path stroke="#56665A" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M54.585 33H24.939c-2.968 0-5.683 1.608-7.01 4.153l-.742 1.423C16.606 39.689 17.45 41 18.75 41H66m-37 0v-8m11 8v-8m35 16h5m-38-3h3M23 59a7 7 0 1 1-14.002-.002A7 7 0 0 1 23 59z"/>
+                  <path stroke="#56665A" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M18 59a2 2 0 1 1-4.001-.001A2 2 0 0 1 18 59zm50 0a2 2 0 1 1-4.001-.001A2 2 0 0 1 68 59z"/>
+                  <path stroke="#56665A" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M73 59a7 7 0 1 1-14.002-.002A7 7 0 0 1 73 59z"/>
+                  <path fill="#56665A" d="M79.667 67H2.333C1.597 67 1 66.553 1 66c0-.553.597-1 1.333-1h77.334c.737 0 1.333.447 1.333 1 0 .553-.596 1-1.333 1"/>
+                  <path fill="#FFF" stroke="#1DBB3E" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 41C9.954 41 1 32.046 1 21S9.954 1 21 1s20 8.954 20 20-8.954 20-20 20"/>
+                  <path stroke="#1DBB3E" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 8.143V21h12.857M21 33.857v1.429M6.714 21h1.429"/>
+                </g>
+              </svg>
+            </div>
+            
+            <h3 class="work-how-item__title">Return</h3>
+            
+            <p class="work-how-item__desc">When your rental contract is over, you can start a new one or return the car. It’s up to you!</p>
+          
+          </div>
+        
+        </div>
+      
+      </div>
+    </section>
+    <section class="index-cars">
+      <div class="index-cars__wrapper">
+        <header class="index-cars__header">
+          <div class="index-cars__title">Our Cars</div>
+          <div class="index-cars__desc">More cars coming soon!</div>
+        </header>
+        <div class="index-cars-item index-cars-item--reverse">
+          <div class="index-cars-item__thumb">
+            <img class="index-cars-item__img" src="pic/index-cars-2-nobg.jpg" srcset="pic/index-cars-2-nobg@2x.jpg 2x" alt="Hyundai IONIQ">
+          </div>
+          <div class="index-cars-item__info">
+            <div class="index-cars-item__info-wrapper">
+              <div class="index-cars-item__title">Hyundai IONIQ Electric</div>
+              <ul class="index-cars-item__list">
+                <li class="index-cars-item__list-item">4 Doors</li>
+                <li class="index-cars-item__list-item">Seats 5</li>
+                <li class="index-cars-item__list-item">140 Miles Per Charge</li>
+                <li class="index-cars-item__list-item">5mi charge/hr slow charger included</li>
+                <li class="index-cars-item__list-item">20 minutes to fast charge to 75% ($.25/minute)</li>
+                <li class="index-cars-item__list-item">Bluetooth</li>
+              </ul>
+            </div>
+          </div>
+        </div>
+        <div class="index-cars-item index-cars-item--reverse">
+          <div class="index-cars-item__thumb">
+            <img class="index-cars-item__img" src="pic/index-cars-2-nobg.jpg" srcset="pic/index-cars-2-nobg@2x.jpg 2x" alt="Hyundai IONIQ">
+          </div>
+          <div class="index-cars-item__info">
+            <div class="index-cars-item__info-wrapper">
+              <div class="index-cars-item__title">Hyundai IONIQ Hybrid</div>
+              <ul class="index-cars-item__list">
+                <li class="index-cars-item__list-item">4 Doors</li>
+                <li class="index-cars-item__list-item">Seats 5</li>
+                <li class="index-cars-item__list-item">60mpg</li>
+                <li class="index-cars-item__list-item">600 miles/tank</li>
+                <li class="index-cars-item__list-item">Bluetooth</li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <section class="work-pricing">
+      <div class="work-pricing__wrapper">
+        <div class="work-pricing__container">
+          
+          <div class="work-pricing__icon">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 82 58">
+              <g fill="none" fill-rule="evenodd" stroke="#FFF" stroke-linecap="round" stroke-linejoin="round" stroke-width="2">
+                <path d="M78.037 57H3.963A2.956 2.956 0 0 1 1 54.053V3.947A2.956 2.956 0 0 1 3.963 1h74.074A2.956 2.956 0 0 1 81 3.947v50.106A2.956 2.956 0 0 1 78.037 57z"/>
+                <path d="M17.5 19h-6a1.5 1.5 0 0 1-1.5-1.5v-6a1.5 1.5 0 0 1 1.5-1.5h6a1.5 1.5 0 0 1 1.5 1.5v6a1.5 1.5 0 0 1-1.5 1.5zM66 36c-1.803 0-3.4.812-4.5 2.069C60.4 36.81 58.803 36 57 36a6 6 0 0 0 0 12c1.803 0 3.4-.812 4.5-2.069C62.6 47.19 64.197 48 66 48a6 6 0 0 0 0-12zM10 48h33m-33-6h9m-8-15v-1m6 1v-1m6 1v-1m18 1v-1m18 1v-1m-30 1v-1m42 1v-1m-36 1v-1m12 1v-1m6 1v-1m12 1v-1M25 42h9"/>
+              </g>
+            </svg>
+          </div>
+          
+          <div class="work-pricing__textual">
+            <h2 class="work-pricing__title">Pricing</h2>
+            <p class="work-pricing__desc">Starting at <b>$22/day</b> covers the 60mpg car, insurance (individual and ride-share) and maintenance. We're the best deal around. Check for yourself.</p>
+          </div>
+        
+        </div>
+      </div>
+    </section>
+  
+  
+    
+    <section class="work-bottom">
+      <div class="work-bottom__wrapper">
+      
+        <div class="work-sign-up">
+          
+          <h2 class="work-sign-up__title">Driver Sign Up</h2>
+          
+          <p class="work-sign-up__desc">If accepted, we’ll reach out and set up a time for your interview and orientation.</p>
+          
+          <a class="work-sign-up-btn" href="/work-signup.html">
+            <span class="work-sign-up-btn__title">Get Started Now</span>
+    
+            <svg class="work-sign-up-btn__icon">
+              <use xlink:href="#icon-classic-arrow"></use>
+            </svg>
+          </a>
+          
+        </div>
+        
+        <div class="work-bottom__line"></div>
+        
+        <div class="work-question">
+          
+          <h2 class="work-question__title">Got Questions?</h2>
+          
+          <p class="work-question__desc">Feel free to <a href="mailto:support@waivecar.com">reach out</a> to us and we'll be happy to answer any questions you may have.</p>
+          
+        </div>
+      
+      </div>
+    </section>
+  
+  </div>
+
+<?  include('footer.php'); ?>
